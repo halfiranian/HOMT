20a435f3264be153ee7a84171a5f94d707aa8c12
Halfiranian <jamessadri@gmail.com>
2011-08-19 21:25:02 +0100

    added user authentication with devise

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
+gem 'geokit'
+gem 'devise'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Mytips::Application.routes.draw do
+  devise_for :users
+
   resources :restaurants
 
+  root :to => "restaurants#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
